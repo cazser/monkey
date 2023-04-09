bc70c069a8e2a76755ea37be4aecf2f9fe2bd997
cazser <2264712072@qq.com>
2023-04-10 06:23:55 +0800

    parser添加returnStatement

--- parser/parser.go
+++ parser/parser.go
@@ -63,6 +63,8 @@ func (p *Parser) parseStatement() ast.Statement{
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement();
+	case token.RETURN:
+		return p.parseReturnStatement();
 	default:
 		return nil;
 	}
@@ -111,5 +113,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool{
 
 }
 
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken();
+
+	for !p.curTokenIs(token.SEMICOLON){
+		p.nextToken();
+	}
+
+	return stmt;
+}
 
 
