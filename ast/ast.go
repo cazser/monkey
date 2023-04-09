2ead19c08e66203b38ab20d6034afa6d6d00aa82
cazser <2264712072@qq.com>
2023-04-10 07:05:00 +0800

    Node添加String

--- ast/ast.go
+++ ast/ast.go
@@ -1,8 +1,14 @@
 package ast
 
-import "monkey/token"
+import (
+  "monkey/token"
+	"bytes"
+)
+
+
 type Node interface{
 	TokenLiteral() string
+	String() string
 }
 
 
@@ -30,6 +36,16 @@ func (p *Program) TokenLiteral() string{
 }
 
 
+func (p *Program) String() string{
+	var out bytes.Buffer;
+	for _, s:= range p.Statements{
+		out.WriteString(s.String());
+
+	}
+	return out.String();
+}
+
+
 type LetStatement struct{
 	Token token.Token
 	Name *Identifier 
@@ -44,6 +60,17 @@ type ReturnStatement struct{
 func (ls *LetStatement) statementNode(){}
 
 func (ls *LetStatement) TokenLiteral() string{ return ls.Token.Literal}
+
+type ExpressionStatement struct{
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode(){}
+
+func (es *ExpressionStatement) TokenLiteral() string{
+   return es.Token.Literal;
+}
 
 type Identifier struct{
 	Token token.Token
@@ -66,3 +93,45 @@ func (rs *ReturnStatement) statementNode(){
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal;
 }
+
+
+func (ls *LetStatement) String() string{
+	var out bytes.Buffer;
+	 
+	out.WriteString(ls.TokenLiteral() + " ");
+	out.WriteString(ls.Name.String() );
+	out.WriteString( " = " );
+
+	if ls.Value != nil{
+		out.WriteString(ls.Value.String());
+	}
+
+	out.WriteString(";");
+
+	return out.String();
+}
+
+func (rs *ReturnStatement) String() string{
+	var out bytes.Buffer;
+	 
+	out.WriteString(rs.TokenLiteral() + " ");
+	
+	if rs.ReturnValue != nil{
+		out.WriteString(rs.ReturnValue.String());
+	}
+
+	out.WriteString(";");
+
+	return out.String();
+}
+
+
+func (es *ExpressionStatement) String() string{
+	if es.Expression != nil{
+		return es.Expression.String();
+	}
+
+	return "";
+}
+
+func (i *Identifier) String() string {return i.Value}
