e0f192a34c3d7f53968851a4b28e0e6eb9e6451b
cazser <2264712072@qq.com>
2023-04-09 15:58:54 +0800

    step4: 添加true,false,return,if,else关键字

--- token/token.go
+++ token/token.go
@@ -26,14 +26,25 @@ const (
 	RPAREN = ")"
 	LBRACE = "{"
 	RBRACE = "}"
+	//关键字
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+  TRUE = "true"
+	FALSE = "false"
+	IF = "if"
+	ELSE = "else"
+	RETURN = "return"
 )
 
 
 var keywords = map[string] TokenType{
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType{
