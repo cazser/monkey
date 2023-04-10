1a2dc4c5837c94d8c8cc6b46f882c98c1c1a779c
cazser <2264712072@qq.com>
2023-04-10 15:52:02 +0800

    添加TestFunctionLiteralParsing测试代码

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -107,7 +107,7 @@ func TestIdentifierExpression(t *testing.T){
  }
  stmt, ok := program.Statements[0].(*ast.ExpressionStatement);
  if !ok{
-	t.Fatalf("program.Statements[0] is not ast.ExprssionStatement. got =%T",
+	t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got =%T",
    program.Statements[0]);
  }
 
@@ -656,6 +656,53 @@ func TestIfElseExpression(t *testing.T) {
 	}
 }
 
+
+func TestFuctionLiteralParsing(t *testing.T){
+	input:= `fn(x, y){ x + y;}`;
+	l:= lexer.New(input);
+	p:= New(l);
+
+	program:= p.ParseProgram();
+	checkParserErrors(t, p);
+
+	if len(program.Statements) != 1{
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+	1, len(program.Statements));
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement);
+	if !ok{
+		t.Fatalf("programing.Statements[0] is not ast.ExpressionStatement. got=%T",
+	program.Statements[0]);
+	}
+
+	function, ok:= stmt.Expression.(*ast.FunctionLiteral);
+  if !ok{
+		t.Fatalf("stmt.Expression is not ast.FunctionLiteral. got=%T",
+	stmt.Expression);
+	}
+
+	if len(function.Parameters)!=2{
+		t.Fatalf("function literal parameters wrong. want 2, got=%d\n",
+	   len(function.Parameters));
+	}
+
+	testLiteralExpression(t, function.Parameters[0], "x");
+	testLiteralExpression(t, function.Parameters[1], "y");
+
+	if len(function.Body.Statements) !=1{
+		t.Fatalf("fucntion.Body.Statements has not 1 statements. got=%d\n",
+	  len(function.Body.Statements))
+	}
+
+	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement);
+	if !ok{
+		t.Fatalf("function body stmt is not ast.ExpressionStatement. got=%T",
+	function.Body.Statements[0]);
+	}
+
+	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y");
+}
 
 
 
