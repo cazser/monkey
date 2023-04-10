88f519676ace5d38e103e42f69c6df7fa1717f36
cazser <2264712072@qq.com>
2023-04-10 10:11:08 +0800

    +1

--- parser/parser.go
+++ parser/parser.go
@@ -19,6 +19,35 @@ const (
 	CALL
 )
 
+var precedences = map[token.TokenType]int{
+   token.EQ 				:    	EQUALS,
+ 	 token.NOT_EQ 		:    	EQUALS,
+ 	 token.LT 				:    	LESSGREATER,
+ 	 token.GT 		    :     LESSGREATER,
+   token.PLUS 			:    	SUM,
+ 	 token.MINUS 		  :    	SUM,
+ 	 token.SLASH 			:    	PRODUCT,
+ 	 token.ASTERISK 	:     PRODUCT,
+
+}
+
+func (p *Parser) peekPrecedence() int{
+    if p, ok:= precedences[p.peekToken.Type]; ok{
+			return p;
+		}
+
+		return LOWEST;
+}
+
+
+func (p *Parser) curPrecedence() int{
+	if p, ok:= precedences[p.peekToken.Type]; ok{
+			return p;
+		}
+
+		return LOWEST;
+}
+
 type Parser struct{
 	l *lexer.Lexer;
 	curToken token.Token;
