f0bd15a435f26011e90d46eae4a9f41fdaeb105f
cazser <2264712072@qq.com>
2023-04-11 06:32:20 +0800

    Object add Null value type

--- object/object.go
+++ object/object.go
@@ -10,6 +10,7 @@ type Object interface{
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 type Integer struct{
 	Value int64
@@ -32,3 +33,10 @@ func (b *Boolean) Type() ObjectType {return BOOLEAN_OBJ}
 func (b *Boolean) Inspect() string {
 	return fmt.Sprintf("%t", b.Value);
 }
+
+type Null struct{
+
+}
+
+func (n *Null) Type() ObjectType {return NULL_OBJ}
+func (n *Null) Inspect() string {return "null"}
