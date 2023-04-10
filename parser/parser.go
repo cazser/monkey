6149e980c82097e0d63fc1459544747b70babefc
cazser <2264712072@qq.com>
2023-04-10 16:16:21 +0800

    测试通过

--- parser/parser.go
+++ parser/parser.go
@@ -78,6 +78,7 @@ func New(l *lexer.Lexer) *Parser{
 	p.regiterPrefix(token.FALSE, p.parseBoolean);
 	p.regiterPrefix(token.LPAREN, p.parseGroupedExpression);
   p.regiterPrefix(token.IF, p.parseIfExpression);
+	p.regiterPrefix(token.FUNCTION, p.parseFunctionLiteral);
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn);
 	p.regiterInfix(token.PLUS, p.parseInfixExpression);
@@ -349,3 +350,49 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement{
 
 	return block;
 }
+
+func (p *Parser) parseFunctionLiteral() ast.Expression {
+	lit := &ast.FunctionLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+
+	return lit
+}
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	identifiers := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return identifiers
+	}
+
+	p.nextToken()
+
+	ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	identifiers = append(identifiers, ident)
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+		identifiers = append(identifiers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return identifiers
+}
+
