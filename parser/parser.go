521f04e042e174c4073eb6eb9a9662f5cdcc3994
cazser <2264712072@qq.com>
2023-04-10 08:08:29 +0800

    parser添加数字字面量

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
@@ -41,6 +42,7 @@ func New(l *lexer.Lexer) *Parser{
 	}
   p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.regiterPrefix(token.IDENT, p.parseIdentifier);
+	p.regiterPrefix(token.INT, p.parseInteger);
 	p.nextToken();
 	p.nextToken();
 
@@ -182,4 +184,20 @@ func (p *Parser) parseExpression(precedence int) ast.Expression{
 func (p *Parser) parseIdentifier() ast.Expression{
 
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseInteger() ast.Expression{
+	lit:= &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil{
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal);
+		p.errors = append(p.errors, msg);
+		return nil;
+	
+	}
+
+	lit.Value =  value;
+
+	return lit;
 }
