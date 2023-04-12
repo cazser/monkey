ff8fa579a9ed6f224291ad913ae0379c9740a2b6
cazser <2264712072@qq.com>
2023-04-12 09:45:22 +0800

    测试通过

--- parser/parser.go
+++ parser/parser.go
@@ -17,6 +17,7 @@ const (
 	PRODUCT     // *
 	PREFIX      // -X or !X
 	CALL        // myFunction(X)
+	INDEX
 )
 
 var precedences = map[token.TokenType]int{
@@ -29,6 +30,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 type (
@@ -75,9 +77,10 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	
 
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
-
+  p.registerInfix(token.LBRACKET, p.parseIndexExpression);
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -466,4 +469,18 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression{
 	}
 
 	return list;
+}
+
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression{
+	exp:= &ast.IndexExpression{Token: p.curToken, Left:left}
+
+	p.nextToken();
+	exp.Index = p.parseExpression(LOWEST);
+
+	if !p.expectPeek(token.RBRACKET){
+		return nil;
+	}
+
+	return exp;
 }
